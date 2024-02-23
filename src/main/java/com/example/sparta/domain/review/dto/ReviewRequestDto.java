da72dca8117117498c037478846f1de12970b638
parkjungsub <eses565@naver.com>
2024-02-24 04:34:03 +0900

    :fix: Timestamped 관련 변수 추가

--- src/main/java/com/example/sparta/domain/review/dto/ReviewRequestDto.java
+++ src/main/java/com/example/sparta/domain/review/dto/ReviewRequestDto.java
@@ -1,9 +1,16 @@
 package com.example.sparta.domain.review.dto;
 
+import java.time.LocalDateTime;
+import java.util.Locale;
 import lombok.Getter;
+import org.springframework.cglib.core.Local;
 
 @Getter
 public class ReviewRequestDto {
-        private Integer rating;
-        private String content;
+
+    private Integer rating;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAy;
+
 }
--- src/main/java/com/example/sparta/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/com/example/sparta/domain/review/dto/ReviewResponseDto.java
@@ -1,17 +1,23 @@
 package com.example.sparta.domain.review.dto;
 
 import com.example.sparta.domain.review.entity.Review;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 public class ReviewResponseDto {
+
     private Long reviewId;
     private Integer rating;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
-    public ReviewResponseDto(Review review){
+    public ReviewResponseDto(Review review) {
         this.reviewId = review.getReviewId();
         this.rating = review.getRating();
         this.content = review.getContent();
+        this.createdAt = review.getCreatedAt();
+        this.modifiedAt = review.getModifiedAt();
     }
 }
