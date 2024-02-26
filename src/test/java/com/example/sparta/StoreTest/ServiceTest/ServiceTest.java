4d2f2c871a9d9cd5263c015919bd57795e84c4b6
sehun <harrisbang2@gmail.com>
2024-02-26 18:07:36 +0900

    Test : 스토어 ServiceTest done

--- src/test/java/com/example/sparta/StoreTest/ServiceTest/ServiceTest.java
+++ src/test/java/com/example/sparta/StoreTest/ServiceTest/ServiceTest.java
@@ -7,20 +7,18 @@ import com.example.sparta.domain.store.repository.StoreRepository;
 import com.example.sparta.domain.store.service.StoreService;
 import com.example.sparta.domain.user.entity.User;
 import com.example.sparta.domain.user.repository.UserRepository;
-import org.awaitility.Awaitility;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -41,12 +39,6 @@ public class ServiceTest {
         StoreRequestDto requestDto = new StoreRequestDto();
         requestDto.setName("service name");
         requestDto.setCategory("Chicken");
-        requestDto.setDibsCount(100);
-        requestDto.setRating(5.5f);
-        requestDto.setAddress("service test land");
-        requestDto.setDeliveryAddress("delivers everywhere darling");
-        requestDto.setReviewCount(34);
-        requestDto.setContent("content");
 
         // 유저
         User user = new User();
@@ -55,18 +47,21 @@ public class ServiceTest {
         user.setEmail("sparta@iscool.com");
         user.setName("스탄이");
 
+        //score
+        Store score = new Store(requestDto,user);
+
         //service
         StoreService storeService = new StoreService(MockStoreRepository,userRepository);
 
         // when
-        given(MockStoreRepository.save(any())).willReturn(new Store());
+        given(MockStoreRepository.save(any())).willReturn(score);
 
         //then
         StoreResponseDto storeResponseDto = storeService.createStore(requestDto,user);
 
-        assertEquals(user.getUserId(),storeResponseDto.getStoreId());
+        assertEquals(score.getName(),storeResponseDto.getName());
     }
-    //given(userRepository.findById(1L)).willReturn(Optional.of(user));
+
     @Test
     @DisplayName("스토어 리스트 가져오기")
     void test2(){
@@ -99,5 +94,144 @@ public class ServiceTest {
         assertEquals(storeResponseDto.size(),ls.size());
     }
 
+    @Test
+    @DisplayName("스토어 수정 하기")
+    void test3(){
+        //given
+        //스토어 request
+        StoreRequestDto requestDto = new StoreRequestDto();
+        requestDto.setName("new name");
+        requestDto.setCategory("Chicken");
+        requestDto.setDibsCount(100);
+        requestDto.setRating(5.5f);
 
+        // 유저
+        User user = new User();
+        user.setUserId(1L);
+        user.setAddress("로마");
+        user.setEmail("sparta@iscool.com");
+        user.setName("스탄이");
+
+        //score
+        Store score = new Store(requestDto,user);
+
+        //service
+        StoreService storeService = new StoreService(MockStoreRepository,userRepository);
+
+        // when
+        given(MockStoreRepository.findById(1L)).willReturn(Optional.of(score));
+        Long id = storeService.editStore(1L,requestDto,user);
+        //then
+        assertEquals(1L,id);
+    }
+    @Test
+    @DisplayName("스토어 삭제 하기")
+    void test4(){
+        //given
+
+        // 유저
+        User user = new User();
+        user.setUserId(1L);
+        user.setAddress("로마");
+        user.setEmail("sparta@iscool.com");
+        user.setName("스탄이");
+
+        //service
+        StoreService storeService = new StoreService(MockStoreRepository,userRepository);
+
+        // when
+        given(MockStoreRepository.findById(1L)).willReturn(Optional.of(new Store()));
+        Long id = storeService.deleteStore(1L,user);
+        //then
+        assertEquals(1L,id);
+    }
+    @Test
+    @DisplayName("스토어 삭제 하기")
+    void test5(){
+        //given
+
+        // 유저
+        User user = new User();
+        user.setUserId(1L);
+        user.setAddress("로마");
+        user.setEmail("sparta@iscool.com");
+        user.setName("스탄이");
+
+        //service
+        StoreService storeService = new StoreService(MockStoreRepository,userRepository);
+
+        // when
+        given(MockStoreRepository.findById(1L)).willReturn(Optional.of(new Store()));
+        Long id = storeService.deleteStore(1L,user);
+        //then
+        assertEquals(1L,id);
+    }
+
+    @Test
+    @DisplayName("스토어 선택 검색 하기")
+    void test6(){
+        //given
+        //스토어 request
+        StoreRequestDto requestDto = new StoreRequestDto();
+        requestDto.setName("new name");
+        requestDto.setCategory("Chicken");
+        requestDto.setDibsCount(100);
+        requestDto.setRating(5.5f);
+
+        // 유저
+        User user = new User();
+        user.setUserId(1L);
+        user.setAddress("로마");
+        user.setEmail("sparta@iscool.com");
+        user.setName("스탄이");
+
+        //score
+        Store score = new Store(requestDto,user);
+
+        //service
+        StoreService storeService = new StoreService(MockStoreRepository,userRepository);
+
+        // when
+        given(MockStoreRepository.findById(1L)).willReturn(Optional.of(score));
+        StoreResponseDto storeResponseDto = storeService.getStoreById(1L);
+        //then
+        assertEquals(storeResponseDto.getName(),score.getName());
+    }
+    @Test
+    @DisplayName("스토어 이름으로 검색 하기")
+    void test7(){
+        //given
+        //스토어 request
+        StoreRequestDto requestDto = new StoreRequestDto();
+        requestDto.setName("new name");
+        requestDto.setCategory("Chicken");
+        requestDto.setDibsCount(100);
+        requestDto.setRating(5.5f);
+
+
+        // 유저
+        User user = new User();
+        user.setUserId(1L);
+        user.setAddress("로마");
+        user.setEmail("sparta@iscool.com");
+        user.setName("스탄이");
+
+        //score
+        Store score = new Store(requestDto,user);
+
+        //ls
+        List<Store> ls = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            ls.add(score);
+        }
+
+        //service
+        StoreService storeService = new StoreService(MockStoreRepository,userRepository);
+
+        // when
+        given(MockStoreRepository.findAllByNameContains("new name")).willReturn(ls);
+        List<StoreResponseDto> storeResponseDtoLs = storeService.getAllStoreByName("new name");
+        //then
+        assertEquals(storeResponseDtoLs.get(1).getName(),ls.get(1).getName());
+    }
 }
