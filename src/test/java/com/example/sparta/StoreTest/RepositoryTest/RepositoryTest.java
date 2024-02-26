1c7f2a5217b0d28d2ff05175fdb8feb33e52f9fc
sehun <harrisbang2@gmail.com>
2024-02-26 18:13:39 +0900

    test#(스토어) : repo test init

--- src/test/java/com/example/sparta/StoreTest/RepositoryTest/RepositoryTest.java
+++ src/test/java/com/example/sparta/StoreTest/RepositoryTest/RepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.sparta.StoreTest.RepositoryTest;
 
 import com.example.sparta.domain.store.repository.StoreRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -12,5 +14,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 public class RepositoryTest {
     @Autowired
     StoreRepository commentRepository;
+    @Test
+    @DisplayName("생성 테스트")
+    void test1(){
 
+    }
 }
