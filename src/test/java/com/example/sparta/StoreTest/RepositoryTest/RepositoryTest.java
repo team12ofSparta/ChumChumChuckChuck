81f30cbb2cb3ec8884ce04834a777b8ecc29e415
sehun <harrisbang2@gmail.com>
2024-02-26 18:11:49 +0900

    feat(스토어 컨트롤러) : Store 컨트롤러 유저 받아오기 추가.

--- src/test/java/com/example/sparta/StoreTest/RepositoryTest/RepositoryTest.java
+++ src/test/java/com/example/sparta/StoreTest/RepositoryTest/RepositoryTest.java
@@ -1,4 +1,16 @@
 package com.example.sparta.StoreTest.RepositoryTest;
 
+import com.example.sparta.domain.store.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE,
+        connection = EmbeddedDatabaseConnection.H2)
 public class RepositoryTest {
+    @Autowired
+    StoreRepository commentRepository;
+
 }
