af21beb2b41e3c323f3824593d937a230b05390f
parkjungsub <eses565@naver.com>
2024-02-27 12:34:04 +0900

    :update: @RequestMappgin("/reviews") 추가

--- src/main/java/com/example/sparta/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/sparta/domain/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.example.sparta.domain.review.dto.ReviewResponseDto;
 import com.example.sparta.domain.review.entity.Review;
 import com.example.sparta.domain.review.service.ReviewService;
 import com.example.sparta.global.dto.ResponseDto;
+import com.example.sparta.global.impl.UserDetailsImpl;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Objects;
@@ -13,22 +14,25 @@ import javax.swing.text.html.parser.Entity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     //모든 리뷰조회
-    @GetMapping("/reviews")
+    @GetMapping
     public ResponseEntity<ResponseDto<List<ReviewResponseDto>>> allReviews() {
         List<ReviewResponseDto> all = reviewService.findAll();
 
@@ -41,8 +45,8 @@ public class ReviewController {
     }
 
     //단건 리뷰조회
-    @GetMapping("/reviews/{reviewId}")
-    public ResponseEntity<ResponseDto<ReviewResponseDto>> OneReview(@PathVariable Long reviewId) {
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ResponseDto<ReviewResponseDto>> oneReview(@PathVariable Long reviewId) {
         ReviewResponseDto reviewResponseDto = reviewService.findOne(reviewId);
 
         return ResponseEntity.ok().body(ResponseDto.
@@ -54,7 +58,7 @@ public class ReviewController {
     }
 
     //리뷰수정
-    @PutMapping("/reviews/{reviewId}")
+    @PutMapping("/{reviewId}")
     public ResponseEntity<ResponseDto<ReviewResponseDto>> updateReview(@PathVariable Long reviewId,
         @RequestBody ReviewRequestDto reviewRequestDto) {
         ReviewResponseDto reviewResponseDto = reviewService.updateOne(reviewId, reviewRequestDto);
@@ -69,7 +73,7 @@ public class ReviewController {
     }
 
     //리뷰 등록
-    @PostMapping("/reviews")
+    @PostMapping
     public ResponseEntity<ResponseDto<ReviewResponseDto>> registerReview(
         @RequestBody ReviewRequestDto reviewRequestDto) {
         ReviewResponseDto register = reviewService.register(reviewRequestDto);
@@ -84,7 +88,7 @@ public class ReviewController {
     }
 
     //리뷰 삭제
-    @DeleteMapping("/reviews/{reviewId}")
+    @DeleteMapping("/{reviewId}")
     public ResponseEntity<ResponseDto<ReviewResponseDto>> deleteReview(
         @PathVariable Long reviewId) {
         reviewService.deleteOne(reviewId);
