ce5b8164ec8cf11b40d13f24cdd8cbf8343380ff
mine <mineki94@naver.com>
2024-02-28 15:27:30 +0900

    ✨ update : UserProfileUpdateResponseDto 테스트 코드 추가

--- src/main/java/com/example/sparta/domain/user/dto/UserProfileUpdateResponseDto.java
+++ src/main/java/com/example/sparta/domain/user/dto/UserProfileUpdateResponseDto.java
@@ -1,11 +1,13 @@
 package com.example.sparta.domain.user.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class UserProfileUpdateResponseDto {
 
     private String name;
--- src/test/java/com/example/sparta/userTest/dtoTest/UserProfileUpdateResponseDtoTest.java
+++ src/test/java/com/example/sparta/userTest/dtoTest/UserProfileUpdateResponseDtoTest.java
@@ -1,5 +1,27 @@
 package com.example.sparta.userTest.dtoTest;
 
+import com.example.sparta.domain.user.dto.UserProfileUpdateResponseDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserProfileUpdateResponseDtoTest {
+    @Test
+    @DisplayName("유저 정보 수정 완료 응답 데이터 테스트")
+    void UserProfileUpdateResponseDto() {
+        //given
+        String name = "테스터";
+        String email = "test@test.com";
+        String address = "테스트 주소";
+        //when
+        UserProfileUpdateResponseDto userProfileUpdateResponseDto = new UserProfileUpdateResponseDto(
+            name, email, address);
+
+        //then
+        Assertions.assertEquals(name, userProfileUpdateResponseDto.getName());
+        Assertions.assertEquals(email, userProfileUpdateResponseDto.getEmail());
+        Assertions.assertEquals(address, userProfileUpdateResponseDto.getAddress());
+
+    }
 
 }
