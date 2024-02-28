b47725c4e6fe305af66ee9eafeaf7a7465b04485
DAchoMI <tyrnsorl123@naver.com>
2024-02-28 18:06:20 +0900

    :sparkles: Menu 추가 API  구현

--- src/main/java/com/example/sparta/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/sparta/domain/menu/repository/MenuRepository.java
@@ -1,8 +1,12 @@
 package com.example.sparta.domain.menu.repository;
 
 import com.example.sparta.domain.menu.entity.Menu;
+import com.example.sparta.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
+    @Query("select m from Menu m where m.name = ?1 and m.store = ?2")
+    boolean findByMenuNameAndStore(String menuName, Store store);
 }
--- src/main/java/com/example/sparta/domain/menu/service/MenuService.java
+++ src/main/java/com/example/sparta/domain/menu/service/MenuService.java
@@ -1,8 +1,12 @@
 package com.example.sparta.domain.menu.service;
 
+import com.example.sparta.domain.menu.dto.AddMenuRequestDto;
+import com.example.sparta.domain.menu.dto.AddMenuResponseDto;
 import com.example.sparta.domain.menu.dto.GetMenuResponseDto;
 import com.example.sparta.domain.menu.entity.Menu;
 import com.example.sparta.domain.menu.repository.MenuRepository;
+import com.example.sparta.domain.store.entity.Store;
+import com.example.sparta.domain.store.repository.StoreRepository;
 import com.example.sparta.domain.user.entity.User;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
 
     public GetMenuResponseDto getMenu(Long storeId, Long menuId) {
 
@@ -26,5 +31,22 @@ public class MenuService {
 
         return GetMenuResponseDto.builder().menuId(menuId).menuName(menu.getName())
             .menuPrice(menu.getPrice()).build();
+    }
+
+    public AddMenuResponseDto addMenu(Long storeId, AddMenuRequestDto requestDto) {
+
+        Store store = storeRepository.findById(storeId).orElseThrow(
+            () -> new NoSuchElementException("해당 가게를 찾을 수 없습니다.")
+        );
+
+        if (menuRepository.findByMenuNameAndStore(requestDto.getMenuName(), store)) {
+            throw new IllegalArgumentException("해당 가게에서 메뉴 이름이 이미 존재합니다.");
+        }
+
+        Menu menu = menuRepository.save(
+            new Menu(requestDto.getMenuName(), requestDto.getMenuPrice(), store));
+
+        return AddMenuResponseDto.builder().menuId(menu.getMenuId()).menuName(menu.getName())
+            .menuPrice(menu.getPrice()).storeId(storeId).storeName(store.getName()).build();
     }
 }
