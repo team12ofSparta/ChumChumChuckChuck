2e1be6c23c4e22fd6d41e63390d305310daf2e17
mine <mineki94@naver.com>
2024-02-28 15:56:07 +0900

    ✨ update : 미사용 코드 삭제

--- src/main/java/com/example/sparta/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/sparta/domain/user/repository/UserRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByName(String username);
 
     Optional<User> findByEmail(String email);
+
     Optional<User> findByKakaoId(Long kakaoId);
 }
