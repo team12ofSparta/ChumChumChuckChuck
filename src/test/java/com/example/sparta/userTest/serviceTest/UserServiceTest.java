2e597d625622b6f464914cb8f778fa50b81c294b
mine <mineki94@naver.com>
2024-02-28 12:49:09 +0900

    ✨ update : 회원정보 수정 테스트 (성공, 실패)

--- src/test/java/com/example/sparta/userTest/serviceTest/UserServiceTest.java
+++ src/test/java/com/example/sparta/userTest/serviceTest/UserServiceTest.java
@@ -1,18 +1,16 @@
 package com.example.sparta.userTest.serviceTest;
 
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 
-import com.example.sparta.domain.user.controller.UserController;
+import com.example.sparta.domain.user.dto.UserProfileUpdateRequestDto;
+import com.example.sparta.domain.user.dto.UserProfileUpdateResponseDto;
 import com.example.sparta.domain.user.dto.UserSignupRequestDto;
 import com.example.sparta.domain.user.dto.UserSignupResponseDto;
 import com.example.sparta.domain.user.entity.User;
 import com.example.sparta.domain.user.entity.UserRoleEnum;
 import com.example.sparta.domain.user.repository.UserRepository;
 import com.example.sparta.domain.user.service.UserService;
-import com.example.sparta.global.dto.ResponseDto;
-import com.example.sparta.global.impl.UserDetailsImpl;
 import com.example.sparta.global.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
@@ -23,8 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,18 +29,20 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
     @Mock
-    private  UserRepository userRepository;
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
     @Mock
-    private  PasswordEncoder passwordEncoder;
-    @Mock
-    private  JwtUtil jwtUtil;
+    private JwtUtil jwtUtil;
     @Mock
     User user;
 
+    @Mock
+    HttpServletResponse httpServletResponse;
 
 
     @Test
-    @DisplayName("유저 회원 가입 성공 테스트")
+    @DisplayName("유저 회원 가입 테스트 - 성공")
     public void userSignupTest01() {
         //given
         String name = "이름";
@@ -61,35 +59,86 @@ public class UserServiceTest {
         UserSignupResponseDto response = userService.userSignup(userSignupRequestDto);
 
         //then
-        Assertions.assertEquals(name,response.getName());
-        Assertions.assertEquals(email,response.getEmail());
-        Assertions.assertEquals(address,response.getAddress());
+        Assertions.assertEquals(name, response.getName());
+        Assertions.assertEquals(email, response.getEmail());
+        Assertions.assertEquals(address, response.getAddress());
     }
 
     @Test
-    @DisplayName("유저 회원 가입 실패 테스트 - 이메일 중복")
+    @DisplayName("유저 회원 가입 테스트 - 실패 - 이메일 중복")
     public void userSignupTest02() {
         //given
         String name = "이름";
         String password = "123456789";
-        String email = "test1test1com";
+        String email = "test@test.com";
         String address = "테스트 주소";
 
         UserSignupRequestDto userSignupRequestDto = new UserSignupRequestDto(
             name, password, email, address
         );
-        user = new User(name,password,email,address, UserRoleEnum.USER,1L);
-        given(userRepository.findByEmail(userSignupRequestDto.getEmail())).willReturn(Optional.of(user));
+        user = new User(name, password, email, address, UserRoleEnum.USER, 1L);
+        given(userRepository.findByEmail(userSignupRequestDto.getEmail())).willReturn(
+            Optional.of(user));
 
         //when
 
-        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,()-> userService.userSignup(userSignupRequestDto));
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,
+            () -> userService.userSignup(userSignupRequestDto));
 
         //then
-       Assertions.assertEquals(e.getMessage(),"이미 가입된 email 입니다");
+        Assertions.assertEquals(e.getMessage(), "이미 가입된 email 입니다");
 
     }
 
+    @Test
+    @DisplayName("회원 정보 수정 테스트 - 성공")
+    public void userProfileUpdateTest01() {
+        //given
+        String name = "이름";
+        String password = "123456789";
+        String email = "test@test.com";
+        String address = "테스트 주소";
+
+        String nameUp = "수정 이름";
+        String addressUp = "수정 주소";
+
+        UserProfileUpdateRequestDto userProfileUpdateRequestDto = new UserProfileUpdateRequestDto(
+            nameUp, addressUp);
+        user = new User(name, password, email, address, UserRoleEnum.USER, 1L);
+
+        given(userRepository.findByEmail(user.getEmail())).willReturn(Optional.of(user));
+        //when
+        UserProfileUpdateResponseDto response = userService.userProfileUpdate(
+            userProfileUpdateRequestDto, user);
+        //then
+        Assertions.assertEquals(nameUp, response.getName());
+        Assertions.assertEquals(email, response.getEmail());
+        Assertions.assertEquals(addressUp, response.getAddress());
+
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 테스트 - 실패 - 로그인 안하고 진입할때")
+    public void userProfileUpdateTest02() {
+        //given
+        String name = "이름";
+        String password = "123456789";
+        String email = "test@test.com";
+        String address = "테스트 주소";
+
+        String nameUp = "수정 이름";
+        String addressUp = "수정 주소";
+
+        UserProfileUpdateRequestDto userProfileUpdateRequestDto = new UserProfileUpdateRequestDto(
+            nameUp, addressUp);
+
+        //when
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,
+            () -> userService.userProfileUpdate(userProfileUpdateRequestDto, user));
+        //then
+        Assertions.assertEquals(e.getMessage(), "로그인 유저 정보가 없습니다.");
+
+    }
 
 
 }
