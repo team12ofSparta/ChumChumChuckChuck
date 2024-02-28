ce61efa6cfed1fd289cdba9b5cfaa290e78d64a5
mine <mineki94@naver.com>
2024-02-28 12:48:27 +0900

    ✨ update : 회원가입 테스트를 위해 사용할 코드 추가

--- src/main/java/com/example/sparta/domain/user/dto/UserSignupResponseDto.java
+++ src/main/java/com/example/sparta/domain/user/dto/UserSignupResponseDto.java
@@ -10,7 +10,6 @@ import lombok.Getter;
 public class UserSignupResponseDto {
 
     private String name;
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String email;
     private String address;
 
