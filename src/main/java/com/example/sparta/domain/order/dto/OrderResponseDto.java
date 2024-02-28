f4475766c1577ae69dadf1e53755621b7ecd8ccc
dbfjru <dbfjru@gmail.com>
2024-02-28 17:14:59 +0900

    fix : OrderResponseDto 수정
    OrderDetailResponseBucket으로 하나의 각각의 메뉴 별로 묶어서 출력

--- src/main/java/com/example/sparta/domain/order/dto/OrderResponseDto.java
+++ src/main/java/com/example/sparta/domain/order/dto/OrderResponseDto.java
@@ -7,33 +7,25 @@ import lombok.Getter;
 
 @Getter
 public class OrderResponseDto {
-
     private final Long orderId;
-    private final List<Long> orderDetailIdList;
-    private final List<String> menuNameList;
-    private final List<Integer> menuQuantityList;
-    private final Long totalPrice;
-    private final String requests;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
-    private final Integer orderStatus;
     private final Long userId;
     private final Long storeId;
+    private final Long totalPrice;
+    private final String requests;
+    private final Integer orderStatus;
+    private final List<OrderDetailResponseBucket> responseBucketList;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
-    public OrderResponseDto(Order order, List<Long> orderDetailIdList, List<String> menuNameList,
-        List<Integer> menuQuantityList) {
+    public OrderResponseDto(Order order, List<OrderDetailResponseBucket> responseBucketList) {
         this.orderId = order.getOrderId();
-
-        this.orderDetailIdList = orderDetailIdList;
-        this.menuNameList = menuNameList;
-        this.menuQuantityList = menuQuantityList;
-
-        this.totalPrice = order.getTotalPrice();
-        this.requests = order.getRequests();
-        this.createdAt = order.getCreatedAt();
-        this.modifiedAt = order.getModifiedAt();
-        this.orderStatus = order.getOrderStatus();
         this.userId = order.getUser().getUserId();
         this.storeId = order.getStore().getStoreId();
+        this.totalPrice = order.getTotalPrice();
+        this.requests = order.getRequests();
+        this.orderStatus = order.getOrderStatus();
+        this.responseBucketList = responseBucketList;
+        this.createdAt = order.getCreatedAt();
+        this.modifiedAt = order.getModifiedAt();
     }
 }
